3d64eff85aa6d09add84d88a9737dfc9a66926e1
Kacper Hernacki <kacperhernacki@Kacpers-MacBook-Pro.local>
2022-08-22 19:46:23 +0200

    feat(transactions): setup for api

--- src/components/TransactionModal.tsx
+++ src/components/TransactionModal.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import styled from "styled-components";
 import { Box, Button, Modal } from "../theme/layout/common";
 import { useForm, Resolver } from "react-hook-form";
+import axios from "axios";
 
 interface TransactionModalProps {
   isOpen: boolean;
@@ -12,6 +13,10 @@ type FormValues = {
   to: string;
   amount: number;
   type: string;
+};
+
+type CreateTransactionResponse = {
+  data: FormValues;
 };
 
 const resolver: Resolver<FormValues> = async (values) => {
@@ -38,7 +43,34 @@ export function TransactionModal({
     formState: { errors },
   } = useForm<FormValues>({ resolver });
 
-  const onSubmit = handleSubmit((data) => console.log(data));
+  async function createTransaction({ formData }: any) {
+    console.log(formData);
+    try {
+      const data = axios.post<CreateTransactionResponse>(
+        "http://localhost:1337/api/transact",
+        { formData },
+        {
+          headers: {
+            Accept: "application/json",
+          },
+        }
+      );
+
+      return data;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.log("error message: ", error.message);
+        return error.message;
+      } else {
+        console.log("unexpected error: ", error);
+        return "An unexpected error occurred";
+      }
+    }
+  }
+
+  const onSubmit = handleSubmit((formData) => {
+    createTransaction({ formData });
+  });
 
   return (
     <Modal open={isOpen} onClose={handleClose}>
@@ -48,6 +80,8 @@ export function TransactionModal({
           {errors?.to && <p>{errors.to.message}</p>}
 
           <input {...register("amount")} placeholder="10" type="number" />
+
+          <input {...register("type")} placeholder="quick-transaction" />
 
           <Button type="submit">Transact</Button>
         </Form>
